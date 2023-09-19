49ce820f1b18f2e86fca6b9c863cc6ec86d9fd52
mohitdudhat <355mohitdudhat@gmail.com>
2023-09-19 12:10:17 +0530

    PR 9

---
+++ PR 9/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Dashboard from './Page/Dashboard';
+import './live.css';
+function App() {
+  return (
+    <html>
+    <body>
+      <div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+      </div>
+      <Dashboard />
+    </body>
+  </html>
+  );
+}
+
+export default App;
---
+++ PR 9/src/data/index.js
@@ -0,0 +1,12 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Mohit',
+    lastName: 'Dudhat',
+    email: 'Mohit@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  }
+];
+
+export { employeesData };
